3eaa32fedc5c29262da33b345ba45157d5a0096f
Florian Durrieu <f.durrieu.ext@meetic-corp.com>
2017-12-04 15:15:23 +0100

    refacto: one line when possible

--- navigate/login.py
+++ navigate/login.py
@@ -1,13 +1,8 @@
 from selenium.webdriver.support.ui import Select
 
 def login(driver, conf):
-    loginButton = driver.find_element_by_id('loginBtn')
-    loginButton.click()
-    login = driver.find_element_by_id('usernameLogin')
-    login.send_keys(conf['login'])
-    pwd = driver.find_element_by_id('passwordLogin')
-    pwd.send_keys(conf['password'])
-    universes = Select(driver.find_element_by_id('serverLogin'))
-    universes.select_by_visible_text(conf['univers'])
-    loginButton = driver.find_element_by_id('loginSubmit')
-    loginButton.click()
+    driver.find_element_by_id('loginBtn').click()
+    driver.find_element_by_id('usernameLogin').send_keys(conf['login'])
+    driver.find_element_by_id('passwordLogin').send_keys(conf['password'])
+    Select(driver.find_element_by_id('serverLogin')).select_by_visible_text(conf['univers'])
+    driver.find_element_by_id('loginSubmit').click()
